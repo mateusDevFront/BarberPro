f871f5b7602fe6760be35fabc9898949e7d5ee7d
mateusdevfront <mateusdsgn2001@gmail.com>
2022-09-22 20:39:57 -0300

    feature/configAPI

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
+import { destroyCookie } from "nookies";
+import Router from 'next/router';
 
 interface AuthContextData {
   user: UserProps;
@@ -25,10 +27,21 @@ interface SignInProps {
 }
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  console.log("ERROR LOGOUT");
+  try {
+    destroyCookie(null, "@barber.token", { path: "/" });
+    Router.push('/login')
+  } catch (err) {
+    console.log("erro ao deslogar");
+  }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
 
+  //Função de login
   async function signIn({ email, password }: SignInProps) {
     console.log({
       email,
