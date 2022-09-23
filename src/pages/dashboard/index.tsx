e385e4b0d0b34d4a65d0e1ac10582b8ed7da0fe7
mateusdevfront <mateusdsgn2001@gmail.com>
2022-09-22 21:43:46 -0300

    feature/canSSRAuth authentication rotas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import {Flex, Text} from '@chakra-ui/react';
+import {canSSRAuth} from '../../utils/canSSRAuth';
 
 export default function Dashboard(){
     return (
@@ -12,4 +13,13 @@ export default function Dashboard(){
             </Flex>
         </>
     )
+}
+
+export const getServerSideProps = () => {
+    console.log('teste')
+    return {
+        props: {
+
+        }
+    }
 }
