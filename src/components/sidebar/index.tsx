2eea43f61593b52cdc8b862d6f460c56e15e6424
mateusdevfront <mateusdsgn2001@gmail.com>
2022-09-26 15:44:42 -0300

    feature/siderbar ajustes

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -29,18 +29,36 @@ const LinkItems: Array<LinkItemProps> = [
   { name: "Minha conta", icon: FiSettings, route: "/haircuts" },
 ];
 
-export function Sidebar({children}: {children: ReactNode}) {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+export function Sidebar({ children }: { children: ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return <Box minH="100vh" bg="barber.900">
-    <SidebarContent
-    onClose={() => onClose}
-    display={{base: 'none', md: 'block'}}
-    />
-    <Box>
+  return (
+    <Box minH="100vh" bg="barber.900">
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+        onClose={onClose}
+      >
+        <DrawerContent>
+          <SidebarContent onClose={() => onClose()} />
+        </DrawerContent>
+      </Drawer>
+
+      <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
+
+      <Box ml={{base: '0', md: 60,}} p={4}>
         {children}
+        </Box>
     </Box>
-  </Box>;
+  );
 }
 
 interface SidebarProps extends BoxProps {
@@ -59,57 +77,122 @@ const SidebarContent = ({ onClose, ...rest }) => {
     >
       <Flex h="20" alignItems="center" justifyContent="space-between" mx="8">
         <Link href="/dashboard">
-            <Flex cursor="pointer" userSelect="none" flexDirection="row">
-                <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" color="#fff">Barber</Text>
-                <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" color="#F1B90A">Pro</Text>
-            </Flex>
+          <Flex cursor="pointer" userSelect="none" flexDirection="row">
+            <Text
+              fontSize="2xl"
+              fontFamily="monospace"
+              fontWeight="bold"
+              color="#fff"
+            >
+              Barber
+            </Text>
+            <Text
+              fontSize="2xl"
+              fontFamily="monospace"
+              fontWeight="bold"
+              color="#F1B90A"
+            >
+              Pro
+            </Text>
+          </Flex>
         </Link>
-        <CloseButton display={{base: 'flex', md: 'none'}} onClick={onClose}/>
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
 
-      {LinkItems.map(link => (
+      {LinkItems.map((link) => (
         <NavItem
           color="#fff"
-        icon={link.icon} route={link.route} key={link.name}>
-            {link.name}
+          icon={link.icon}
+          route={link.route}
+          key={link.name}
+        >
+          {link.name}
         </NavItem>
       ))}
-
     </Box>
   );
 };
 interface NavItemProps extends FlexProps {
-    icon: IconType;
-    children: ReactNode;
-    route: string;
+  icon: IconType;
+  children: ReactNode;
+  route: string;
 }
-const NavItem = ({icon, children, route, ...rest}: NavItemProps) => {
-    return(
-        <Link href={route} style={{textDecoration: 'none'}}>
-        <Flex
-            align="center"
-            p="4"
-            m="4"
-            borderRadius="lg"
-            role="group"
-            cursor="pointer"
-            _hover={{
-                bg: 'barber.900',
-                color: '#fff'
+const NavItem = ({ icon, children, route, ...rest }: NavItemProps) => {
+  return (
+    <Link href={route} style={{ textDecoration: "none" }}>
+      <Flex
+        align="center"
+        p="4"
+        m="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "barber.900",
+          color: "#fff",
+        }}
+        {...rest}
+      >
+        {icon && (
+          <Icon
+            mr={4}
+            fontSize="16"
+            as={icon}
+            _groupHover={{
+              color: "#fff",
             }}
-            {...rest}>
-                {icon && (
-                    <Icon
-                        mr={4}
-                        fontSize="16"
-                        as={icon}
-                        _groupHover={{
-                            color: '#fff'
-                        }}
-                    />
-                )}
-                {children}
-        </Flex>
+          />
+        )}
+        {children}
+      </Flex>
     </Link>
-    )
+  );
+};
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void;
 }
+//Mobile
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      bg="gray.900"
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent="flex-start"
+      {...rest}
+    >
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        color="gray.100"
+        aria-label="open menu"
+        icon={<FiMenu color="white"/>}
+      />
+      <Flex flexDirection="row">
+        <Text
+          ml="8"
+          fontSize="2xl"
+          fontFamily="monospace"
+          fontWeight="bold"
+          color="#fff"
+        >
+          Barber
+        </Text>
+        <Text
+          fontSize="2xl"
+          fontFamily="monospace"
+          fontWeight="bold"
+          color="#F1B90A"
+        >
+          Pro
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
